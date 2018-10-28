b5fffc954a7e33e4ab5b02b31190fb41e1605e6c
Tommi Mäklin <tommi.maklin@helsinki.fi>
2018-10-28 18:44:04 +0200

    add option to pass parameters differently

--- src/rcg.cpp
+++ src/rcg.cpp
@@ -131,3 +131,74 @@ Matrix<double> rcg_optl_mat(const Matrix<double> &logl, const Sample &sample, co
   logsumexp(gamma_Z, gamma_Z);
   return(gamma_Z);
 }
+
+Matrix<double> rcg_optl_mat(const Matrix<double> &logl, const long unsigned &total_counts, const std::vector<long unsigned> &ec_counts, const std::vector<double> &alpha0, const double &tol, unsigned maxiters) {
+  unsigned n_rows = logl.get_rows();
+  unsigned n_cols = logl.get_cols();
+  Matrix<double> gamma_Z(n_rows, n_cols, 1.0);
+  Matrix<double> gamma_new(n_rows, n_cols, 0.0);
+  Matrix<double> oldstep(n_rows, n_cols, 0.0);
+  Matrix<double> step(n_rows, n_cols, 0.0);
+  double oldnorm = 1.0;
+
+  Matrix<double> q_Z(n_rows, n_cols, 0.0);
+  logsumexp(gamma_Z, q_Z);
+  long double bound = -100000.0;
+  bool didreset = false;
+
+  double bound_const = std::accumulate(alpha0.begin(), alpha0.end(), 0.0);;
+  bound_const += total_counts;
+  bound_const = -lgamma(bound_const);
+  bound_const += std::accumulate(alpha0.begin(), alpha0.end(), 0.0, [](double a, double b) { return a + lgamma(b); });
+
+  std::vector<double> N_k(alpha0.size());
+  q_Z.right_multiply(ec_counts, N_k);
+  std::transform(N_k.begin(), N_k.end(), alpha0.begin(), N_k.begin(), std::plus<double>());
+
+  for (unsigned k = 0; k < maxiters; ++k) {
+    double newnorm = mixt_negnatgrad(q_Z, gamma_Z, N_k, logl, step);
+    double beta_FR = newnorm/oldnorm;
+    oldnorm = newnorm;
+
+    if (didreset) {
+      oldstep *= 0.0;
+    } else if (beta_FR > 0) {
+      oldstep *= beta_FR;
+      step += oldstep;
+    }
+    didreset = false;
+    
+    gamma_new.sum_fill(gamma_Z, step); // gamma_new = gamma_Z + step
+    logsumexp(gamma_new, q_Z);
+    q_Z.right_multiply(ec_counts, N_k);
+    std::transform(N_k.begin(), N_k.end(), alpha0.begin(), N_k.begin(), std::plus<double>());
+    long double oldbound = bound;
+    bound = bound_const;
+    ELBO_rcg_mat(logl, q_Z, gamma_new, ec_counts, alpha0, N_k, bound);
+
+    if (bound < oldbound) {
+      didreset = true;
+      gamma_Z += step;
+      if (beta_FR > 0) {
+	gamma_Z -= oldstep;
+      }
+      logsumexp(gamma_Z, q_Z);
+      q_Z.right_multiply(ec_counts, N_k);
+      std::transform(N_k.begin(), N_k.end(), alpha0.begin(), N_k.begin(), std::plus<double>());
+      bound = bound_const;
+      ELBO_rcg_mat(logl, q_Z, gamma_Z, ec_counts, alpha0, N_k, bound);
+    } else {
+      oldstep = step;
+      gamma_Z = gamma_new;
+    }
+    if (k % 5 == 0) {
+      std::cerr << "  " <<  "iter: " << k << ", bound: " << bound << ", |g|: " << newnorm << std::endl;
+    }
+    if (bound - oldbound < tol && !didreset) {
+      logsumexp(gamma_Z, gamma_Z);
+      return(gamma_Z);
+    }
+  }
+  logsumexp(gamma_Z, gamma_Z);
+  return(gamma_Z);
+}
